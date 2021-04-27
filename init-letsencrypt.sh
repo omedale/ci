2dea96bd09f95ecf126228b280e05affa8c068d1
Oluwafemi Medale <omedale@gmail.com>
2021-04-27 18:48:47 +0100

    config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api.mentorci.online)
+domains=(thawing-woodland-78827.herokuapp.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ngeneanthony@gmail.com" # Adding a valid address is strongly recommended
